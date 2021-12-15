239411d7cea8705abc58cfdea00ea67099899e56
ryota-murakami <dojce1048@gmail.com>
2021-12-16 03:00:37 +0900

    migrate tailwindCSS 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['active'],
-      opacity: ['disabled'],
     },
   },
   plugins: [require('@tailwindcss/forms')],
