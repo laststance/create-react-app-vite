53372183e59c7a590a3016a2a5b5f0d6bf8651f8
ryota-murakami <dojce1048@gmail.com>
2021-12-17 02:01:24 +0900

    new ReactDOMSPI

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// @ts-ignore v18 @types coming not yet
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
