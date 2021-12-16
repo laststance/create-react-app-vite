229a4d93a36f9cb9c367dcfc5de3d70fc4a47817
ryota-murakami <dojce1048@gmail.com>
2021-12-17 00:54:14 +0900

    add fetch tst

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,7 +1,9 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.get('/docs', (req, res, ctx) => {
-    return res(ctx.body())
+  rest.get('/docs_list', (req, res, ctx) => {
+    const data = [['Axios', 'https://axios-http.com/]']]
+
+    return res(ctx.json(data))
   }),
 ]
---
+++ src/fetchDocs.test.ts
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+test('fetch docs', async () => {
+  const response = await axios.get('/docs_list')
+  expect(response.data).toEqual([['Axios', 'https://axios-http.com/]']])
+})
