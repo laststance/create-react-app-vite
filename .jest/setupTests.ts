3edf29ad26c21009cc9b8f8d7c2732cbb1720176
ryota-murakami <dojce1048@gmail.com>
2021-12-28 17:12:51 +0900

    suppress RTL waring msg

--- .jest/setupTests.ts
+++ .jest/setupTests.ts
@@ -4,7 +4,23 @@ import '@testing-library/jest-dom'
 import 'whatwg-fetch'
 import { server } from '../mocks/server'
 
-beforeAll(() => server.listen())
+
+// eslint-disable-next-line no-console
+const originalError = console.error
+beforeAll(
+  () => {
+    server.listen()
+
+// eslint-disable-next-line no-console
+    console.error = (...args) => {
+      // Suppress jest error msg
+      if (args[0].includes('ReactDOM.render is no longer supported in React 18.')) {
+        return
+      }
+      originalError.call(console, ...args)
+    }
+  }
+)
 
 beforeAll(() => {
   server.resetHandlers()
@@ -14,4 +30,7 @@ afterEach(() => server.resetHandlers())
 
 afterAll(() => {
   server.close()
+
+  // eslint-disable-next-line no-console
+  console.error = originalError
 })
