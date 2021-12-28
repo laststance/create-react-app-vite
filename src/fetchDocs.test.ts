1c10516d74a704a3c0279e994fb68aedde602af3
ryota-murakami <dojce1048@gmail.com>
2021-12-29 00:20:33 +0900

    fix test

--- src/fetchDocs.test.ts
+++ src/fetchDocs.test.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 test('fetch docs', async () => {
   const response = await axios.get('/docs_list')
-  expect(response.data).toEqual([['Axios', 'https://axios-http.com/]']])
+  expect(response.data).toEqual(['Axios', 'https://axios-http.com/]'])
 })
