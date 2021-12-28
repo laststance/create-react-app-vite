ec0624d9ed3db6f2e78485e771f0e646c2bbed6b
ryota-murakami <dojce1048@gmail.com>
2021-12-28 17:35:36 +0900

    msw setup mockServiceWorker

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+if (process.env.NODE_ENV === 'development') {
+  import('../mocks/browser').then(({ worker }) => {
+    worker.start()
+  })
+}
+
 // @ts-ignore v18 @types coming not yet
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
