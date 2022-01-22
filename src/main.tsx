72f71e1a1405a377bdc17e95126d1903451e19ca
ryota-murakami <dojce1048@gmail.com>
2022-01-23 00:00:51 +0900

    finally msw work with vite

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-if (process.env.NODE_ENV === 'development') {
-  import('../mocks/browser').then(({ worker }) => {
-    worker.start()
-  })
+const runReact = () => {
+  // @ts-ignore v18 @types coming not yet
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(<App />)
 }
 
-// @ts-ignore v18 @types coming not yet
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+if (process.env.NODE_ENV === 'development') {
+  import('../mocks/browser')
+    .then(({ worker }) => {
+      worker.start()
+    })
+    .then(() => runReact())
+} else {
+  runReact()
+}
