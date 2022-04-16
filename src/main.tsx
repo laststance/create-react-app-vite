8ee618114abf75f83e56076563c039c75c7b5975
ryota-murakami <dojce1048@gmail.com>
2022-04-16 23:34:37 +0900

    update ReactDOM type

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import App from './App'
 
-// @ts-ignore v18 @types not coming yet
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 if (process.env.NODE_ENV === 'development') {
   import('../mocks/browser')
