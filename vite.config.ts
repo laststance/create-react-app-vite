1ea92b9f2b1cb961eedb372bd199ca219e354ea0
ryota-murakami <dojce1048@gmail.com>
2022-05-15 11:16:35 +0900

    add eslint-plugin-sort-keys-fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     sourcemap: true,
   },
+  plugins: [react()],
   server: {
-    open: true,
     host: true,
+    open: true,
   },
 })
