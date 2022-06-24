3012062c86400ceda1a6165f92ee8ced7fa1d448
ryota-murakami <dojce1048@gmail.com>
2022-06-24 16:09:40 +0900

    remove old testing message surpression

--- jest/setupTests.ts
+++ jest/setupTests.ts
@@ -4,22 +4,8 @@ import '@testing-library/jest-dom'
 import 'whatwg-fetch'
 import { server } from '../mocks/server'
 
-// eslint-disable-next-line no-console
-const originalError = console.error
 beforeAll(() => {
   server.listen()
-
-  // eslint-disable-next-line no-console
-  console.error = (...args) => {
-    // Suppress jest error msg
-    if (
-      args[0].includes('ReactDOM.render is no longer supported in React 18.')
-    ) {
-      return
-    }
-
-    originalError.call(console, ...args)
-  }
 })
 
 beforeAll(() => {
@@ -30,7 +16,4 @@ afterEach(() => server.resetHandlers())
 
 afterAll(() => {
   server.close()
-
-  // eslint-disable-next-line no-console
-  console.error = originalError
 })
