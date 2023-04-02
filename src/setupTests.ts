d2df3523733eaeeef9d3190e5b4d121fee9a554f
ryota-murakami <dojce1048@gmail.com>
2023-04-02 18:46:56 +0900

    tweek

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,8 +3,10 @@ import matchers from '@testing-library/jest-dom/matchers'
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)
 
-import { server } from './mocks/server'
+import { server } from '../mocks/server'
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
-afterAll(() => server.close())
+
 afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
