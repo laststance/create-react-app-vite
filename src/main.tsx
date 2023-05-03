915973bcf540c50928b153e3e3bcb79678c74363
ryota-murakami <dojce1048@gmail.com>
2023-05-03 16:30:04 +0900

    :bulb: add service worker explanation

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
+// Setup MSW mock server in development
 if (process.env.NODE_ENV === 'development') {
+  // Certify MSW's Service Worker is available before start React app.
   import('../mocks/browser')
     .then(({ worker }) => {
       worker.start()
-    })
+    }) // Run <App /> when Service Worker is ready to intercept requests.
     .then(() => {
       root.render(<App />)
     })
-} else {
+  // Never setup MSW mock server in production
+} else if (process.env.NODE_ENV === 'production') {
   root.render(<App />)
 }
