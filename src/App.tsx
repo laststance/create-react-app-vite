0181556a558010b232cdac35e9eee247cb1a0187
ryota-murakami <dojce1048@gmail.com>
2023-06-14 07:03:26 +0900

    fix templete tag sctructure

--- src/App.tsx
+++ src/App.tsx
@@ -22,58 +22,56 @@ const App: React.FC = () => {
 
   return (
     <main className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{process.env.REACT_APP_TEXT}</p>
-        <p>
-          <button
-            type="button"
-            className="h-26 my-4 w-52 rounded border border-solid border-white px-4 py-3"
-            onClick={() => setCount(count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          {docsList.length > 0
-            ? docsList.map((v, i) => {
-                return (
-                  <span key={i}>
-                    {' | '}
-                    <a
-                      className="App-link"
-                      href={v.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {v.name}
-                    </a>
-                  </span>
-                )
-              })
-            : false}
-        </p>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{process.env.REACT_APP_TEXT}</p>
+      <p>
+        <button
+          type="button"
+          className="h-26 my-4 w-52 rounded border border-solid border-white px-4 py-3"
+          onClick={() => setCount(count + 1)}
+        >
+          count is: {count}
+        </button>
+      </p>
+      <p>
+        Edit <code>App.tsx</code> and save to test HMR updates.
+      </p>
+      <p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {' | '}
+        <a
+          className="App-link"
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </a>
+        {docsList.length > 0
+          ? docsList.map((v, i) => {
+              return (
+                <span key={i}>
+                  {' | '}
+                  <a
+                    className="App-link"
+                    href={v.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {v.name}
+                  </a>
+                </span>
+              )
+            })
+          : false}
+      </p>
     </main>
   )
 }
