089f2a3eaacdd2d5e7007532a8e2882a9447f541
ryota-murakami <dojce1048@gmail.com>
2024-02-09 08:04:13 +0900

    update to Vite v5

--- src/@types/utility.d.ts
+++ src/@types/utility.d.ts
@@ -11,16 +11,16 @@ declare global {
   type Expand<T> = T extends (...args: infer A) => infer R
     ? (...args: Expand<A>) => Expand<R>
     : T extends infer O
-    ? { [K in keyof O]: O[K] }
-    : never
+      ? { [K in keyof O]: O[K] }
+      : never
 
   type ExpandRecursively<T> = T extends (...args: infer A) => infer R
     ? (...args: ExpandRecursively<A>) => ExpandRecursively<R>
     : T extends object
-    ? T extends infer O
-      ? { [K in keyof O]: ExpandRecursively<O[K]> }
-      : never
-    : T
+      ? T extends infer O
+        ? { [K in keyof O]: ExpandRecursively<O[K]> }
+        : never
+      : T
 }
 
 declare module 'react' {
