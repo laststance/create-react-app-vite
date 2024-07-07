85f48a1a5785434f9ea0f16f33855729c57182dd
Ryota Murakami <dojce1048@gmail.com>
2024-07-08 08:41:04 +0900

    update react19@RC

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,15 +1,14 @@
-import React, { memo } from 'react'
+import React from 'react'
 import type { PropsWithChildren } from 'react'
 
 import styles from './index.module.css'
 
-const Box: React.FC<PropsWithChildren> = memo(({ children, ...rest }) => {
+const Box: React.FC<PropsWithChildren> = ({ children, ...rest }) => {
   return (
     <div className={styles.box} {...rest}>
       {children}
     </div>
   )
-})
-Box.displayName = 'Box'
+}
 
 export default Box
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,14 @@
 import type { PropsWithChildren } from 'react'
-import React, { memo } from 'react'
+import React from 'react'
 
 import styles from './Layout.module.css'
 
-const Layout: React.FC<PropsWithChildren> = memo(({ children, ...rest }) => {
+const Layout: React.FC<PropsWithChildren> = ({ children, ...rest }) => {
   return (
     <main className={styles.layout} {...rest}>
       <div className={styles.container}>{children}</div>
     </main>
   )
-})
-Layout.displayName = 'Layout'
+}
 
 export default Layout
