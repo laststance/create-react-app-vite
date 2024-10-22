d2bd9a8e0b6fa024c43f473a9c349b6bec745182
Ryota Murakami <dojce1048@gmail.com>
2024-10-23 08:39:38 +0900

    remove use hook

--- src/pages/Index/DocList.tsx
+++ src/pages/Index/DocList.tsx
@@ -1,26 +1,35 @@
-import fetch from 'cross-fetch'
-import React, { memo, use } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import Spinner from '@/components/Spinner'
 
 import styles from './DocList.module.css'
 interface Props {}
 
-const DocList: React.FC<Props> = memo(() => {
-  const docList: DocList = use(
-    fetch('http://localhost:3000/api/doclist').then(async (res) => res.json()),
-  )
+const DocList: React.FC<Props> = () => {
+  const [docList, setDocList] = useState<DocList>([])
+
+  useEffect(() => {
+    fetch('/api/doclist')
+      .then(async (res) => res.json())
+      .then((data) => setDocList(data))
+  }, [])
 
   return (
     <>
       <section className={styles.documentList}>
-        {docList.map((doc, i) => (
-          <a key={i} className={styles.button} href={doc.url} target="_blank">
-            {doc.name}
-          </a>
-        ))}
+        {docList.length === 0 ? (
+          <Spinner />
+        ) : (
+          docList.map((doc, i) => (
+            <a key={i} className={styles.button} href={doc.url} target="_blank">
+              {doc.name}
+            </a>
+          ))
+        )}
       </section>
     </>
   )
-})
+}
 DocList.displayName = 'DocList'
 
 export default DocList
