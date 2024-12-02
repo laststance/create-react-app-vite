aec3405d19fa3a936b227996279b43526937501c
Ryota Murakami <dojce1048@gmail.com>
2024-12-02 10:18:26 +0900

    migrate ReactRouter 6 to 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
 
 import ErrorBoundary from './components/ErrorBoundary'
 import router from './router'
--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router'
 
 import Layout from './components/Layout/Layout'
 import Index from './pages/Index'
