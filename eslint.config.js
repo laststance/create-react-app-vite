90dcc3c11a6392d0f75cbd07e3bca7865338e915
Ryota Murakami <dojce1048@gmail.com>
2025-06-27 12:45:29 +0900

    refactor: update ESLint configuration and module exports
    - Replaced eslint.config.mjs with eslint.config.js using defineConfig from eslint/config.
    - Updated package.json to reflect changes in ESLint configuration and

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'eslint/config'
+import tsPrefixer from 'eslint-config-ts-prefixer'
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export default defineConfig([
+  {
+    ignores: [
+      '**/.vscode',
+      '**/node_modules',
+      '**/build',
+      '**/dist',
+      '**/.github',
+      '**/.idea',
+      'public/mockServiceWorker.js',
+    ],
+  },
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+    },
+  },
+  ...tsPrefixer,
+])
